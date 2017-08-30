0d276d019d69589a08e14ea3fd166bd254cd7572
Daniël Klabbers <daniel@klabbers.email>
2017-08-30 22:27:39 +0200

    still not parsing right

--- migrations/2017_08_30_000000_migrate_to_templates.php
+++ migrations/2017_08_30_000000_migrate_to_templates.php
@@ -12,29 +12,64 @@
  */
 
 
+use Flagrow\Upload\File;
+use Flagrow\Upload\Templates\AbstractTemplate;
 use Illuminate\Database\ConnectionInterface;
+use Flagrow\Upload\Templates;
 
 return [
     'up'   => function (ConnectionInterface $connection) {
-        $replace = [];
-
         $connection
             ->table('posts')
-            ->chunk(10, function ($posts) use (&$replace) {
+            ->chunk(10, function ($posts) use (&$replace, $connection) {
                 foreach ($posts as $post) {
                     $content = $post->content;
-                    replaceIdentifiers($content);
+                    replaceIdentifiers($connection, $content);
 
-                    $replace[$post->id] = $content;
+                    $connection
+                        ->table('posts')
+                        ->where('id', $post->id)
+                        ->update([
+                        'content' => $content
+                    ]);
                 }
         });
-
-        $connection->table('posts')->update()
     },
     'down' => function (ConnectionInterface $connection) {
+    // ..
     }
 ];
 
-function replaceIdentifiers(&content) {
+function replaceIdentifiers(ConnectionInterface $connection, &$content) {
+    $content = preg_replace_callback_array([
+        '/\$image-(?<uuid>[a-z0-9-]{36})/' => function ($m) use ($connection) {
+            return bbcode($connection, $m['uuid'], new Templates\ImageTemplate());
+        },
+        '/\$file-(?<uuid>[a-z0-9-]{36})/' => function ($m) use ($connection) {
+            return bbcode($connection, $m['uuid'], new Templates\FileTemplate());
+        },
+    ], $content);
+}
+function bbcode(ConnectionInterface $connection, $uuid, AbstractTemplate $template) {
+    $file = $connection->table('flagrow_files')->where('uuid', $uuid)->first();
+    $bbcode = $template->bbcode();
+    /** @var \Flarum\Formatter\Formatter $formatter */
+    $formatter = app(\Flarum\Formatter\Formatter::class);
 
+    $code = preg_replace_callback_array([
+        '/\](?<find>.*)\[/' => function ($m) use ($file) {
+            return str_replace($m['find'], $file->base_name, $m[0]);
+        },
+        '/size=(?<find>{.*?})/' => function ($m) use ($file) {
+            return str_replace($m['find'], (new File)->human_filesize($file->size), $m[0]);
+        },
+        '/uuid=(?<find>{.*?})/' => function ($m) use ($file) {
+            return str_replace($m['find'], $file->uuid, $m[0]);
+        },
+        '/url=(?<find>{.*?})/' => function ($m) use ($file) {
+            return str_replace($m['find'], $file->url, $m[0]);
+        }
+    ], $bbcode);
+
+    return $formatter->parse($code);
 }
